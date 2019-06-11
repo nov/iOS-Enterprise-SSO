ea1f00c1c9f04196083489f22e6c587ef5a21a4c
nov <nov@matake.jp>
2019-06-11 11:44:11 +0900

    Initial Commit

---
+++ sso/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sso
+//
+//  Created by nov on 2019/06/11.
+//  Copyright © 2019 YAuth.jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
